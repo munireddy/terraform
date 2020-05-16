ca13fc9c16e49d04eaadb615ab5f16ad1967fc63
munireddy <munisekhar.reddy@gmail.com>
2020-05-16 17:29:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Common change for tf12 .. remove $ and double quotes for  all veriable interpolations
 provider "aws" {
-   region = "${var.region}"
+   region = var.region
 }
 
 terraform {
@@ -16,12 +16,12 @@ resource "aws_instance" "cZServers" {
   #ami = "ami-5e8bb23b"
   ami = "ami-5e8bb23b"
   instance_type = "t2.micro"
-  count = "${var.instance_count}"
+  count = var.instance_count
   vpc_security_group_ids = ["${var.security_group}"]
 
-  key_name = "${var.key_name}"
+  key_name = var.key_name
   
-  user_data = "${file("user-data.txt")}"
+  user_data = file("user-data.txt")
   #Changed for tf12
   tags = {
     Name = "My-terraformInst--${count.index + 1}"
@@ -35,7 +35,7 @@ output "public_ip" {
 }
 
 resource "null_resource" "myPublicIps" {
-count = "${var.instance_count}"
+count = var.instance_count
 provisioner "local-exec" {
       command = "echo '${element(aws_instance.cZServers.*.public_ip, count.index)}' >> hosts1"
 }
